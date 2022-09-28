3f109595f04fa9cda250a53589f7225bedd4849b
Ahmadsabili <ahmadsabili0081@gmail.com>
2022-09-29 01:05:15 +0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,116 +1,147 @@
-class Book{
-  constructor(title,penerbit,time__book){
-    this.title = title;
-    this.penerbit = penerbit;
-    this.time__book = time__book;
-  }
-}
-// add submit event
-let submitBtn = document.querySelector('.btn__submit');
-submitBtn.addEventListener('click',(e) => {
+let formSubmit = document.querySelector('.form__book');
+let inputTitle = document.querySelector('.title');
+let inputPenerbit = document.querySelector('.penerbit');
+let inputTime = document.querySelector('.time__book');
+let elementPar = null;
+let bookList = document.querySelector('.book-list');
+let btn = document.querySelector('.btn');
+
+formSubmit.addEventListener('submit',(e) => {
   e.preventDefault();
-  let title = document.querySelector('.title').value;
-  let penerbit  = document.querySelector('.penerbit').value;
-  let timeBooking = document.querySelector('.time__book').value;
-  let now = new Date();
-  let day = ("0" + now.getDate()).slice(-2);
-  let month = ("0" + (now.getMonth() + 1)).slice(-2);
-  let today = (day)+"-"+(month)+"-"+now.getFullYear();
-  timeBooking.value = today;
-  if(title === '' || penerbit === '' || timeBooking === ""){
-      addBooks.showAlert('Isi field ini dengan benar','danger')      
+  if(!inputTitle.value || !inputPenerbit.value || !inputTime.value){
+    window.alert('Isi form dengan benar, jangan kosong!');
   }else{
-        // memasukkan ke object book
-        let book = new Book(title,penerbit,timeBooking);
-        // menambahkan book ke addbooks class
-        addBooks.addBookToList(book);
-        //ssimpan ke local storage
-        Store.saveBookList(book);
-        // menghapus sebuah value yang telah dibuat
-        addBooks.clearStatic(book)
-        addBooks.showAlert('List booking telah terbuat','success')   
-  }
-})
-class addBooks{
-    static displayBook(){
-      let books = Store.getBooks();
-
-      books.forEach((book) => addBooks.addBookToList(book));
-    }
-    static addBookToList(item){
-      let list = document.querySelector('#book-list')
-      let row = document.createElement('tr');
-       
-      row.innerHTML = `
-       <td>${item.title}</td>
-       <td>${item.penerbit}</td>
-       <td>${item.time__book}</td>
-       <td><a href="#" class="btn btnDelete btn-danger btn-sm delete">Delete</a></td>
-      `;
-      list.appendChild(row);
-    }
-    static clearStatic(book){
-      document.querySelector('.title').value = "";
-      document.querySelector('.penerbit').value = "";
-      document.querySelector('.time__book').value = "";
-    }
-    static deleteElement(hapus){
-      if(hapus.classList.contains('btnDelete')){
-        hapus.parentElement.parentElement.remove();
-        addBooks.showAlert('Field telah terhapus','success')
+    if(elementPar === null){
+      let getId = () => {
+        return Math.floor(Math.random() * 1000);
       }
-    }
-    static showAlert(pesan,classname){
-      let container = document.querySelector('.apps__book');
-      let form = document.querySelector('.form__book');
-      let alert = document.createElement('div');
-      alert.className = `alert alert-${classname}`
-      alert.appendChild(document.createTextNode(pesan))
-      container.insertBefore(alert,form)
-      setTimeout(() => {
-        let alert = document.querySelector('.alert');
-        alert.remove()
-      },1500)
-    }
-}
-document.addEventListener('DOMContentLoaded',addBooks.displayBook);
-// save booklist
-class Store{
-  static saveBookList(book){
-    let books;
-    if(localStorage.getItem('books') === null){
-      books = []
+      let objData = new createObj(inputTitle.value,inputPenerbit.value,inputTime.value,getId());
+      createElement.showElement(objData);
+      local.saveLocalStorage(objData)
+      inputTitle.value = "";
+      inputPenerbit.value = "";
+      inputTime.value = ""
+      elementPar = null;
     }else{
-      books = JSON.parse(localStorage.getItem('books'));
+      createElement.editElsObj()
+      elementPar = null;
     }
-    books.push(book)
-    localStorage.setItem("books", JSON.stringify(books));
   }
-  static getBooks(){
-    let books;
-    if(localStorage.getItem('books') === null){
-      books = []
-    }else{
-      books = JSON.parse(localStorage.getItem('books'))
-    }
-    return books
-  }
-  static removeBooksList(hapus){
-    let removeBook = hapus;
-    let books;
-    if(localStorage.getItem('books') === null){
-      books = []
-    }else{
-      books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.splice(books.indexOf(removeBook), 1);
-    localStorage.setItem('books', JSON.stringify(books))
+});
+
+bookList.addEventListener('click',(e)=>{
+  createElement.deleteRow(e.target);
+  createElement.editEls(e.target);
+});
+
+window.addEventListener('DOMContentLoaded',() => {
+  let getObj = local.getLocalStorage();
+  getObj.forEach((item) => {
+    createElement.showElement(item)
+  })
+})
+class createObj{
+  constructor(inputTitle,inputPenerbit,inputTime,id){
+    this.id = id
+    this.Title = inputTitle;
+    this.Penerbit = inputPenerbit;
+    this.Time = inputTime;
   }
 }
 
-let bookList = document.querySelector('#book-list');
-bookList.addEventListener('click', (e) => {
-  e.preventDefault();
-  addBooks.deleteElement(e.target)
-  Store.removeBooksList(e.target.parentElement.parentElement)
-})
+class createElement{
+  static showElement(data){
+    let tr = document.createElement('tr');
+    tr.innerHTML = `<td>${data.Title}</td>
+                    <td>${data.Penerbit}</td>
+                    <td>${data.Time}</td>
+                    <td>
+                    <button class="edit" type="button">Edit</button>
+                    <button class="delete" type="button">Delete</button>
+                    </td>
+    `;
+    bookList.appendChild(tr);
+
+  }
+  static deleteRow(e){
+    if(e.classList.contains('delete')){
+      let targetRemove = e.parentElement.parentElement;
+      targetRemove.remove();
+      local.deleteLocalStorage(targetRemove);
+    }
+  }
+  static editEls(e){
+    if(e.classList.contains('edit')){
+      btn.innerHTML = "Edit"
+        elementPar = e.parentElement.parentElement;
+        inputTitle.value = elementPar.children[0].textContent;
+        inputPenerbit.value = elementPar.children[1].textContent;
+        inputTime.value = elementPar.children[2].textContent;
+    }
+  }
+  static editElsObj(){
+    let getTitle =  elementPar.children[0].textContent
+    local.updateLocalStorage(getTitle)
+    elementPar.children[0].innerHTML = inputTitle.value;
+    elementPar.children[1].innerHTML = inputPenerbit.value;
+    elementPar.children[2].innerHTML = inputTime.value;
+    btn.innerHTML = "Booking Book"
+    inputTitle.value = "";
+    inputPenerbit.value = "";
+    inputTime.value = "";
+  }
+}
+
+class local{
+  static saveLocalStorage(data){
+    console.log(data)
+    let Books;
+    if(localStorage.getItem('Books') === null){
+      Books = [];
+    }else{
+      Books = JSON.parse(localStorage.getItem('Books'))
+    }
+    Books.push(data);
+    localStorage.setItem('Books',JSON.stringify(Books))
+  }
+  static getLocalStorage(){
+    let Books;
+    if(localStorage.getItem('Books') === null){
+      Books = [];
+    }else{
+      Books = JSON.parse(localStorage.getItem('Books'))
+    }
+    return Books;
+  }
+  static deleteLocalStorage(data){
+    let Books;
+    if(localStorage.getItem('Books') === null){
+      Books = [];
+    }else{
+      Books = JSON.parse(localStorage.getItem('Books'))
+    }
+    let targetDelete = data.children[0].textContent;
+    let getIndex = Books.findIndex((itemIndex) => {
+       return itemIndex.Title === targetDelete
+    });
+    Books.splice(getIndex,1);
+    localStorage.setItem('Books',JSON.stringify(Books))
+  }
+  static updateLocalStorage(data){
+    let Books;
+    if(localStorage.getItem('Books') === null){
+      Books = [];
+    }else{
+      Books = JSON.parse(localStorage.getItem('Books'))
+    }
+    let updateObj =  Books.map((itempUpdate) => {
+        if(itempUpdate.Title === data){
+           itempUpdate.Title = inputTitle.value;
+           itempUpdate.Penerbit = inputPenerbit.value;
+           itempUpdate.Time = inputTime.value;
+        }
+        return  itempUpdate;
+   });
+   localStorage.setItem('Books',JSON.stringify(updateObj))
+  }
+}
